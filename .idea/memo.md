7614a903e992354642bf1ea8beb0ccd4baf110e1
hwkang <landman93@naver.com>
2021-10-18 17:27:31 +0900

    view py 파일 추가

---
+++ .idea/memo.md
@@ -0,0 +1,12 @@
+### 참고
+
+http://117.110.143.27/
+
+
+QtDesigner
+
+C:\Users\KHW\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\LocalCache\local-packages\Python39\site-packages\PySide2
+
+PYQT5 사용법 YOUTUBE 참고하기
+
+https://www.youtube.com/watch?v=rHDWnfsJP4A
--- selectInfo/StandardSelectInfo.py
+++ selectInfo/StandardSelectInfo.py
@@ -5,16 +5,16 @@ from enum import Enum
 # 인쇄 방식
 class PrintMethod(Enum):
     Digital = 1     # 디지털
-    OffSet = 2     # 옵셋
+    OffSet = 2      # 옵셋
 
 
 # 인쇄물 종류
 class PrintKind(Enum):
-    Book = 1
-    Leaflet = 2
-    Flyer = 3
-    Poster = 4
-    ShoppingBack = 5
+    Book = 1            # 책자
+    Leaflet = 2         # 리플렛
+    Flyer = 3           # 전단
+    Poster = 4          # 포스터
+    ShoppingBack = 5    # 쇼핑백
 
 
 ## 규격 선택 정보
